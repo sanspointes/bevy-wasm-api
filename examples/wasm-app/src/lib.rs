aad5f882cd3e003c72b88e9d1107da13a1e37d47
Connor G Meehan <connorgm@pm.me>
2024-02-17 21:00:11 +1100

    refact: Moves some tests around

--- examples/wasm-app/src/lib.rs
+++ examples/wasm-app/src/lib.rs
@@ -39,42 +39,10 @@ struct MyApi;
 #[allow(dead_code)]
 #[bevy_wasm_api]
 impl MyApi {
-    pub fn count_entites(world: &mut World) -> usize {
-        world.query::<Entity>().iter(world).len()
+    pub fn test(_world: &mut World) -> Vec<i32> {
+        todo!();
     }
-    pub fn get_entities(world: &mut World) -> Vec<Entity> {
-        let result: Vec<_> = world.query_filtered::<Entity, With<Name>>().iter(world).collect();
-        result
-    }
-    pub fn set_entity_name(world: &mut World, entity: u32, name: String) -> Result<bool, String> {
-        let mut name_component = world.get_mut::<Name>(Entity::from_raw(entity)).ok_or("Could not find entity".to_string())?;
-        name_component.set(name);
-        Ok(true)
-    }
-    pub fn get_entity_name(world: &mut World, entity: u32) -> Option<String> {
-        world.get::<Name>(Entity::from_raw(entity)).map(|name| name.to_string())
-    }
-
-    pub fn spawn_circle(world: &mut World, x: f32, y: f32, z: f32) -> Entity {
-        let mut sys_state = SystemState::<(
-            Commands,
-            ResMut<Assets<Mesh>>,
-            ResMut<Assets<ColorMaterial>>,
-        )>::new(world);
-
-        let (mut commands, mut meshes, mut materials) = sys_state.get_mut(world);
-        let entity = commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-                material: materials.add(ColorMaterial::from(Color::RED)),
-                transform: Transform::from_translation(Vec3::new(x, y, z)),
-                ..default()
-            },
-            Name::from("Circle"),
-        )).id();
-
-        sys_state.apply(world);
-
-        entity
+    pub fn test_2(_world: &mut World) -> Vec<Option<i32>> {
+        todo!();
     }
 }
