fec31440f41f350d363ea1f43907ca2bceebfa6a
Connor G Meehan <connorgm@pm.me>
2024-02-17 23:11:58 +1100

    feat(deploy): Switches rsw -> wasm-pack, deploys to github actions

--- examples/vite-app/src/main.ts
+++ examples/vite-app/src/main.ts
@@ -1,9 +1,8 @@
-import init, { MyApi, setup_bevy_app } from 'bevy-app';
+import { MyApi, setup_bevy_app } from 'bevy-app';
 import Toolbar from './Toolbar';
 import Sidebar from './Sidebar';
 
 async function main() {
-    await init();
     try {
         setup_bevy_app('#canvas');
     } catch(error) {
--- examples/vite-app/vite.config.ts
+++ examples/vite-app/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import { ViteRsw } from 'vite-plugin-rsw';
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: './',
   plugins: [
-    ViteRsw(),
-  ],
-})
+    wasm(),
+    topLevelAwait()
+  ]
+});
